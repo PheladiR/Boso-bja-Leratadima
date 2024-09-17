ec2b42c4556169394e8e3fd9c4d24cd197daebdf
PheladiR <olgarachoshi@gmail.com>
2024-09-17 16:09:14 +0200

    update js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function refreshWeather(response){
 let temperatureElement = document.querySelector("#temperature");
-let temperature = response.data.temperature.current
+let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 
