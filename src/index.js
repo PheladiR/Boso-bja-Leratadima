640ab6a4c3db7e1c887ada72cb9a490a36599494
PheladiR <olgarachoshi@gmail.com>
2024-09-22 19:51:18 +0200

    created a new function and updated js

--- src/index.js
+++ src/index.js
@@ -59,6 +59,13 @@ if (minutes< 10){
 
 }
 
+function ForecastDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon","Tue","Wed","Thu", "Fri", "Sat"];
+
+  return days[date.getDay()]
+}
+
 function getForecast(city){
    axios(apiUrl).then(displayForecast)
 }
@@ -67,19 +74,25 @@ function getForecast(city){
 function displayForecast(response) {
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if (index<5){
     forecastHtml += `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">
-        <img src = ${day.condition.icon_url}}/></div>
+        <div class="weather-forecast-date">${ForecastDay(day.time)}</div>
+        <div>
+        <img src = ${
+          day.condition.icon_url
+        } class="weather-forecast-icon"/></div>
         <div class="weather-forecast-temperatures">
           <div class="Weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}</strong>
           </div>
-          <div class="Weather-forecast-temperature">${Math.round(day.temperature.minimum)}</div>
+          <div class="Weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}</div>
         </div>
       </div>`;
+        }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
