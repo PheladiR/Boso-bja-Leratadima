7302037fea5737ee4ceea81f3de5b15623f40c21
PheladiR <olgarachoshi@gmail.com>
2024-09-22 08:33:20 +0200

    Updates on js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 function refreshWeather(response) {
-  console.log(response); 
+  console.log(response);
 
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
 }
 
 function searchCity(city) {
@@ -32,3 +38,23 @@ function handleSearchSubmit(event) {
 // Event listener for the search form submission
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = ["Sunday", 
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday"];
+  let day = days[date.getDay()];
+
+if (minutes< 10){
+  minutes =`0${minutes}`
+}
+
+  return `${day} ${hours}:${minutes}`
+
+}
