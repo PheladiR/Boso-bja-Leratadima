43aa862403225f008cda0ef87812d07724f1189f
PheladiR <olgarachoshi@gmail.com>
2024-09-22 19:53:37 +0200

    update js

--- src/index.js
+++ src/index.js
@@ -67,7 +67,9 @@ function ForecastDay(timestamp){
 }
 
 function getForecast(city){
-   axios(apiUrl).then(displayForecast)
+  let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
 }
 
 
